b89c4d9ed3a5066d6a440c463955083b9f543c27
salviuez <salviuez19@gmail.com>
2023-06-19 18:59:05 +0530

    callbackhell

---
+++ script.js
@@ -0,0 +1,31 @@
+console.log("10");
+setTimeout(() => {
+    console.log("9")
+}, 1000)
+setTimeout(() => {
+    console.log("8")
+}, 2000)
+setTimeout(() => {
+    console.log("7")
+}, 3000)
+setTimeout(() => {
+    console.log("6")
+}, 4000)
+setTimeout(() => {
+    console.log("5")
+}, 5000)
+setTimeout(() => {
+    console.log("4")
+}, 6000)
+setTimeout(() => {
+    console.log("3")
+}, 7000)
+setTimeout(() => {
+    console.log("2")
+}, 8000)
+setTimeout(() => {
+    console.log("1")
+}, 9000)
+setTimeout(() => {
+    console.log("Happy Independence Day")
+}, 10000)
